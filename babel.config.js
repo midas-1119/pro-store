bda7542cad92535f3f7ec4feee644525d741b515
SmartCodeTracker <scoderacker@gmail.com>
2019-05-29 16:28:35 +0000

    chore(deps): update dependency typescript to v3.5.1

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          node: "current"
-        }
-      }
-    ],
-    "@babel/preset-react",
-    "@babel/preset-typescript"
-  ]
+    presets: ['@babel/preset-env', '@babel/preset-react', "@babel/preset-typescript"],
 };
--- src/useAsyncFn.ts
+++ src/useAsyncFn.ts
@@ -18,23 +18,17 @@ export type AsyncState<T> =
       value: T;
     };
 
-export default function useAsyncFn<
-  Result = any,
-  Args extends any[] = any[],
-  Fn extends Function = (...args: Args | []) => Promise<Result>
->(
-  fn: Fn,
-  deps: DependencyList = [],
-  initialState: AsyncState<Result> = { loading: false }
-): [AsyncState<Result>, (...args: Args | []) => Promise<Result>] {
-  const [state, set] = useState<AsyncState<Result>>(initialState);
+const useAsyncFn = <T>(fn: (...args: any[]) => Promise<T>, deps: DependencyList = []): [AsyncState<T>, () => void] => {
+  const [state, set] = useState<AsyncState<T>>({
+    loading: false,
+  });
 
   const mounted = useRefMounted();
 
-  const callback = useCallback((...args: Args | []) => {
+  const callback = useCallback((...args) => {
     set({ loading: true });
 
-    return fn(...args).then(
+    fn(...args).then(
       value => {
         if (mounted.current) {
           set({ value, loading: false });
@@ -49,4 +43,6 @@ export default function useAsyncFn<
   }, deps);
 
   return [state, callback];
-}
+};
+
+export default useAsyncFn;
