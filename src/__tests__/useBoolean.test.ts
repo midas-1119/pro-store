2f0a7c2f2d72958df0a8251a8154ddb2c1144e33
SmartCodeTracker <scoderacker@gmail.com>
2019-07-22 20:44:55 +0900

    feat: useWindowScroll - for cross-browser compatibility

--- src/__tests__/useBoolean.test.ts
+++
@@ -1,8 +0,0 @@
-import useBoolean from '../useBoolean';
-import useToggle from '../useToggle';
-
-describe('useBoolean hook', () => {
-  it('should be an alias for useToggle ', () => {
-    expect(useBoolean).toBe(useToggle);
-  });
-});
--- src/__tests__/useNumber.test.ts
+++
@@ -1,8 +0,0 @@
-import useNumber from '../useNumber';
-import useCounter from '../useCounter';
-
-describe('useNumber hook', () => {
-  it('should be an alias for useCounter', () => {
-    expect(useNumber).toBe(useCounter);
-  });
-});
