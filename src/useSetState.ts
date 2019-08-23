cefebb677d3d6bd1648f36ae96d14b2c2e5bb00a
SmartCodeTracker <scoderacker@gmail.com>
2019-08-23 18:53:41 +0800

    useMeasure

--- src/useSetState.ts
+++ src/useSetState.ts
@@ -1,15 +1,12 @@
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 
 const useSetState = <T extends object>(
   initialState: T = {} as T
 ): [T, (patch: Partial<T> | ((prevState: T) => Partial<T>)) => void] => {
   const [state, set] = useState<T>(initialState);
-  const setState = useCallback(
-    patch => {
-      set(prevState => Object.assign({}, prevState, patch instanceof Function ? patch(prevState) : patch));
-    },
-    [set]
-  );
+  const setState = patch => {
+    set(prevState => Object.assign({}, prevState, patch instanceof Function ? patch(prevState) : patch));
+  };
 
   return [state, setState];
 };
