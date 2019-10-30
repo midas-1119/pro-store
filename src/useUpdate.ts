bd4e22fc17a449fdb301ba58c03a0ca8c6e96b3d
SmartCodeTracker <scoderacker@gmail.com>
2019-10-30 16:51:33 +0300

    feat: react-like state resolver to use it in stateful hooks;

--- src/useUpdate.ts
+++ src/useUpdate.ts
@@ -1,11 +1,10 @@
-import { useCallback, useState } from 'react';
-
-const incrementParameter = (num: number): number => ++num;
+import { useState, useCallback } from 'react';
 
 const useUpdate = () => {
   const [, setState] = useState(0);
   // useCallback with empty deps as we only want to define updateCb once
-  return useCallback(() => setState(incrementParameter), []);
+  const updateCb = useCallback(() => setState(cnt => cnt + 1), []);
+  return updateCb;
 };
 
 export default useUpdate;
