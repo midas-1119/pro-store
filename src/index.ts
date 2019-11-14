81e56dbb0a40cbcfb9e378f10958150441a02d03
SmartCodeTracker <scoderacker@gmail.com>
2019-11-14 14:40:01 +0300

    feat(useStateValidator): Refactor method and improve typings;

--- src/index.ts
+++ src/index.ts
@@ -94,4 +94,3 @@ export { useMultiStateValidator } from './useMultiStateValidator';
 export { default as useWindowScroll } from './useWindowScroll';
 export { default as useWindowSize } from './useWindowSize';
 export { default as useMeasure } from './useMeasure';
-export { default as useSet } from './useSet';
