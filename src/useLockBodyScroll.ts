ca9d4372bb3022a0250d50fc0b56c75c9168227d
SmartCodeTracker <scoderacker@gmail.com>
2020-01-10 21:53:07 +0000

    chore(deps): update dependency ts-node to v8.6.1

--- src/useLockBodyScroll.ts
+++ src/useLockBodyScroll.ts
@@ -15,32 +15,14 @@ export function getClosestBody(el: Element | HTMLElement | HTMLIFrameElement | n
   return getClosestBody((el as HTMLElement).offsetParent!);
 }
 
-function preventDefault(rawEvent: TouchEvent): boolean {
-  const e = rawEvent || window.event;
-  // Do not prevent if the event has more than one touch (usually meaning this is a multi touch gesture like pinch to zoom).
-  if (e.touches.length > 1) return true;
-
-  if (e.preventDefault) e.preventDefault();
-
-  return false;
-}
-
 export interface BodyInfoItem {
   counter: number;
   initialOverflow: CSSStyleDeclaration['overflow'];
 }
 
-const isIosDevice =
-  typeof window !== 'undefined' &&
-  window.navigator &&
-  window.navigator.platform &&
-  /iP(ad|hone|od)/.test(window.navigator.platform);
-
 const bodies: Map<HTMLElement, BodyInfoItem> = new Map();
 
 const doc: Document | undefined = typeof document === 'object' ? document : undefined;
-
-let documentListenerAdded = false;
 
 export default !doc
   ? function useLockBodyMock(_locked: boolean = true, _elementRef?: RefObject<HTMLElement>) {}
@@ -58,15 +40,7 @@ export default !doc
         if (locked) {
           if (!bodyInfo) {
             bodies.set(body, { counter: 1, initialOverflow: body.style.overflow });
-            if (isIosDevice) {
-              if (!documentListenerAdded) {
-                document.addEventListener('touchmove', preventDefault, { passive: false });
-
-                documentListenerAdded = true;
-              }
-            } else {
-              body.style.overflow = 'hidden';
-            }
+            body.style.overflow = 'hidden';
           } else {
             bodies.set(body, { counter: bodyInfo.counter + 1, initialOverflow: bodyInfo.initialOverflow });
           }
@@ -74,16 +48,7 @@ export default !doc
           if (bodyInfo) {
             if (bodyInfo.counter === 1) {
               bodies.delete(body);
-              if (isIosDevice) {
-                body.ontouchmove = null;
-
-                if (documentListenerAdded) {
-                  document.removeEventListener('touchmove', preventDefault);
-                  documentListenerAdded = false;
-                }
-              } else {
-                body.style.overflow = bodyInfo.initialOverflow;
-              }
+              body.style.overflow = bodyInfo.initialOverflow;
             } else {
               bodies.set(body, { counter: bodyInfo.counter - 1, initialOverflow: bodyInfo.initialOverflow });
             }
