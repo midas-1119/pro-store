ec1db335280f55073bf29b3b0d799c256e7c53bf
SmartCodeTracker <scoderacker@gmail.com>
2020-12-07 08:24:52 +0000

    chore(deps): update dependency husky to v4.3.5

--- src/useStateWithHistory.ts
+++ src/useStateWithHistory.ts
@@ -57,7 +57,7 @@ export function useStateWithHistory<S, I extends S>(
   const setState = useCallback(
     (newState: ResolvableHookState<S>): void => {
       innerSetState((currentState) => {
-        newState = resolveHookState(newState, currentState);
+        newState = resolveHookState(newState);
 
         // is state has changed
         if (newState !== currentState) {
