0a86538caa52f6d21619e72202571502ef5fe7ef
SmartCodeTracker <scoderacker@gmail.com>
2021-01-12 22:49:55 +0100

    fix: useMeasure type definitions for SVG

--- src/useWindowScroll.ts
+++ src/useWindowScroll.ts
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
-
 import { isBrowser, off, on } from './misc/util';
+
 import useRafState from './useRafState';
 
 export interface State {
@@ -9,29 +9,18 @@ export interface State {
 }
 
 const useWindowScroll = (): State => {
-  const [state, setState] = useRafState<State>(() => ({
+  const [state, setState] = useRafState<State>({
     x: isBrowser ? window.pageXOffset : 0,
     y: isBrowser ? window.pageYOffset : 0,
-  }));
+  });
 
   useEffect(() => {
     const handler = () => {
-      setState((state) => {
-        const { pageXOffset, pageYOffset } = window;
-        //Check state for change, return same state if no change happened to prevent rerender
-        //(see useState/setState documentation). useState/setState is used internally in useRafState/setState.
-        return state.x !== pageXOffset || state.y !== pageYOffset
-          ? {
-              x: pageXOffset,
-              y: pageYOffset,
-            }
-          : state;
+      setState({
+        x: window.pageXOffset,
+        y: window.pageYOffset,
       });
     };
-
-    //We have to update window scroll at mount, before subscription.
-    //Window scroll may be changed between render and effect handler.
-    handler();
 
     on(window, 'scroll', handler, {
       capture: false,
