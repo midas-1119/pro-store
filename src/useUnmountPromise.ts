bb4737b146db9903ebc9d39b34f497650b1317fc
SmartCodeTracker <scoderacker@gmail.com>
2021-01-29 14:18:23 +0000

    chore(deps): update dependency ts-jest to v26.5.0

--- src/useUnmountPromise.ts
+++ src/useUnmountPromise.ts
@@ -1,11 +1,10 @@
-import { useMemo, useRef } from 'react';
-import useEffectOnce from './useEffectOnce';
+import { useMemo, useRef, useEffect } from 'react';
 
 export type Race = <P extends Promise<any>, E = any>(promise: P, onError?: (error: E) => void) => P;
 
 const useUnmountPromise = (): Race => {
   const refUnmounted = useRef(false);
-  useEffectOnce(() => () => {
+  useEffect(() => () => {
     refUnmounted.current = true;
   });
 
